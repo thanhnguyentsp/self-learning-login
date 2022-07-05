8a88853a1a33440b4db5b96445a1ae20b0ce1fdd
thanh <thanh.nguyen@tenpoint7.com>
2022-07-05 17:08:27 +0700

    Init App

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import LoginForm, { FormSubmit } from './components/LoginForm.vue';
+
+const onSumit = ref((e: FormSubmit) => {
+  console.log(e);
+  
+  window.alert(`Logged in with user ${e.name}`)
+})
+</script>
+
+<template>
+  <div class="app w-full flex justify-center items-center">
+    <LoginForm @submit="onSumit"></LoginForm>
+  </div>
+</template>
+
+<style >
+.app {
+  height: 100vh;
+  background-image: url('https://img.freepik.com/free-vector/japanese-wave-line-art-landscape-background-abstract-mountain-banner-design-pattern-vector-illustration-geometric-poster_90220-714.jpg?w=1480&t=st=1657012673~exp=1657013273~hmac=b6533ef118b70bde3538dd160d0bcaa0210099047a483aeadea45fa919584d2c');
+  background-repeat: no-repeat;
+  background-size:cover;
+}
+</style>
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,46 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+export interface FormSubmit {
+    name: string;
+    password: string;
+} 
+const form = ref<FormSubmit>({
+    name: '',
+    password: ''
+});
+
+</script>
+<template>
+  <div>
+    
+    <div class="text-center font-bold text-lg text-indigo-800">Login</div>
+    <div class="md:grid md:grid-cols-3 md:gap-6">
+      <div class="mt-5 md:mt-0 md:col-span-3">
+        <form @submit="$emit('submit', form)" >
+          <div class="shadow sm:rounded-md sm:overflow-hidden">
+            <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
+              <div>
+                <label for="username" class="block text-sm font-medium text-gray-700"> Username </label>
+                <div class="mt-1">
+                  <input v-model="form.name" id="username" name="username" class="p-3 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md" placeholder="john_doe" />
+                </div>
+                <p class="mt-2 text-sm text-gray-500">Fill your username</p>
+              </div>
+            </div>
+            <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
+              <div>
+                <label for="password" class="block text-sm font-medium text-gray-700"> Password </label>
+                <div class="mt-1">
+                  <input v-model="form.password" type="password" id="password" name="password" class="p-3 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md" placeholder="Enter password" />
+                </div>
+              </div>
+            </div>
+            <div class="px-4 py-3 bg-gray-50 text-center sm:px-6 flex justify-center items-center">
+              <button  type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Save</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
